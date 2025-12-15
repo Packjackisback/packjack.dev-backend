f2c116aaaaada9a89117f159122e090eecf91f99
packjack <83289186+Packjackisback@users.noreply.github.com>
2025-12-15 07:36:50 -0600

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3031;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
